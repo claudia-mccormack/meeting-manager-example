fdadafc7f0254b8fc5e55e8e0b345cc8569f60b3
Claudia McCormack <mccormack.chc@gmail.com>
2016-09-11 12:14:34 -0400

    use angular in meetings index view

--- app/assets/javascripts/controllers/meetings_ctrl.js
+++ app/assets/javascripts/controllers/meetings_ctrl.js
@@ -1,7 +1,11 @@
 (function () {
   "use strict";
 
-  angular.module("app").controller("meetingsCtrl", function($scope) {
-    $scope.message = "Hello!";
+  angular.module("app").controller("meetingsCtrl", function($scope, $http) {
+    $scope.setup = function() {
+      $http.get('/api/v1/meetings.json').then(function(response) {
+        $scope.meetings = response.data;
+      });
+    };
   });
 })();
